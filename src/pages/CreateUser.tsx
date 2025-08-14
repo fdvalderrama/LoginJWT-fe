639b549b1694cdce6bed65a96f6618a5fcab45b6
Daniel Valderrama <danimanri05@gmail.com>
2025-08-14 15:47:54 -0600

    Initial commit

--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -1,7 +1,7 @@
 const CreateUser = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-4xl font-bold">Create User</h1>
+      <h1 className="text-4xl font-bold">Create Account</h1>
     </div>
   );
 };
